5bc59b8cb570d6344e9ca4d68fa529219f03fcbb
Tyler Spangler <tyspangler91@gmail.com>
2023-07-07 12:23:04 -0600

    added tests, removed functions

---
+++ bgg_analysis/test.py
@@ -0,0 +1,77 @@
+"""
+Tests for the bgg_analysis files
+"""
+from api_call import build_url, execute_api_call
+import scrape_game_ids
+import sys
+import pandas as pd
+
+
+def test_build_url():
+    """
+    Tests successful run of build url function
+    """
+    test_df = pd.DataFrame(
+        {"game_title": ["Game1", "Game2", "Game3"], "game_id": [1, 2, 3]}
+    )
+    actual_url = build_url(2, test_df)
+    print(actual_url)
+    expected_url = "https://api.geekdo.com/xmlapi/boardgame/1, 2"
+    assert actual_url == expected_url
+    assert isinstance(actual_url, str)
+
+
+def test_execute_api_call(requests_mock):
+    """
+    Tests the excute api method and asserts that the return is dict
+    """
+    resp = """
+    <boardgame objectid="161533">
+    <yearpublished>2017</yearpublished>
+    <minplayers>1</minplayers>
+    <maxplayers>4</maxplayers>
+    <playingtime>120</playingtime>
+    <minplaytime>60</minplaytime>
+    <maxplaytime>120</maxplaytime> 
+    </boardgame>
+    """
+    test_url = "https://fake_api.com"
+    requests_mock.get(test_url, status_code=200, text=resp)
+    actual = execute_api_call(test_url)
+    assert isinstance(actual, dict)
+
+
+def test_api_call_failed(requests_mock):
+    """
+    Tests an unsuccessful api call and asserts that the return is the status code
+
+    """
+    test_url = "https://fake_api.com"
+    requests_mock.get(test_url, status_code=404, text=None)
+    actual = execute_api_call(test_url)
+    assert actual == 404
+
+
+def test_get_title_id():
+    """
+    Tests successful run of building the title/id dataframe
+    """
+    test_list = [
+        "/boardgame/ID1/name1",
+        "/boardgame/ID2/name2",
+        "/boardgame/ID3/name3",
+    ]
+    actual = scrape_game_ids.get_title_id(test_list)
+    expected = pd.DataFrame(
+        {"game_title": ["name1", "name2", "name3"], "game_id": ["ID1", "ID2", "ID3"]}
+    )
+    pd.testing.assert_frame_equal(actual, expected)
+
+
+def test_get_url(requests_mock):
+    test_url = "https://fake_url.com"
+    test_text = "<a href='/boardgame/224517/brass-birmingham' class='primary'>Brass: Birmingham</a>"
+    requests_mock.get(test_url, text=test_text)
+    actual = scrape_game_ids.get_game_urls(test_url)
+    expected = {"/boardgame/224517/brass-birmingham"}
+    assert actual == expected
